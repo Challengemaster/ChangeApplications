74800204ef42215d3b396931f76a78c11d3ca437
sms203 <major8872@gmail.com>
2017-03-05 23:46:26 +1300

    Initial commit

---
+++ app/src/main/java/poswel/com/changeapplications/MainActivity.java
@@ -0,0 +1,37 @@
+package poswel.com.changeapplications;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Define your views
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //find your views
+        imageButton = (ImageButton) findViewById(R.id.image_button);
+
+        //Assign a Listener to your button
+        imageButton.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               // Start your second activity
+                                               Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                                               startActivity(intent);
+
+                                           }
+                                       }
+
+
+        );
+    }
+}
